3469a8c45c36551a55607ce25fbb716e2e1a2c62
catgoesmeow14 <exoexo1990@gmail.com>
2022-01-27 00:32:47 +0800

    fixing bug

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,7 @@
 function addTodo(){
     const textTodo = document.getElementById("title").value;
     const timestamp = document.getElementById("date").value;
-    console.log("todo" + textTodo);
+    console.log("todo " + textTodo);
     console.log("timestamp" + timestamp);
 }
 
